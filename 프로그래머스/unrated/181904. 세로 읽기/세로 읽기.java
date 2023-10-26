87a5b0db914169aa301452cc3055c98a19f39bc6
김지성 <77275224+adk2421@users.noreply.github.com>
2023-10-26 11:08:55 +0900

    [level unrated] Title: 세로 읽기, Time: 11.49 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -2,7 +2,7 @@ class Solution {
     public String solution(String my_string, int m, int c) {
         String answer = "";
         for (int i = 0; i < my_string.length() / m; i++)
-            answer += my_string.substring(m * i, m * i + m).split("")[c - 1];
+            answer += my_string.charAt(m * i + c - 1);
         
         return answer;
     }
