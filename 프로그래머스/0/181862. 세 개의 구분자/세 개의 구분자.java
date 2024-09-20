48f378d370680939d9eb946a876991fae99af455
김지성 <77275224+adk2421@users.noreply.github.com>
2024-09-20 16:30:40 +0900

    [level 0] Title: 세 개의 구분자, Time: 51.24 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,13 +1,6 @@
-import java.util.StringTokenizer;
-
 class Solution {
     public String[] solution(String myStr) {
-        StringTokenizer st = new StringTokenizer(myStr, "abc");
-        String[] answer = new String[st.countTokens()];
-        
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = st.nextToken();
-        }
-        return answer.length == 0 ? new String[] {"EMPTY"} : answer;
+        String[] answer = myStr.replaceAll("[a|b|c]+", " ").trim().split(" ");
+        return answer[0].equals("") ? new String[] {"EMPTY"} : answer;
     }
 }
