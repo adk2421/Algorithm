de1de9fabafe987c8d1ed2b328ff1493faf2ea69
김지성 <77275224+adk2421@users.noreply.github.com>
2024-12-05 10:43:10 +0900

    [level 0] Title: 배열 만들기 6, Time: 280.85 ms, Memory: 419 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -6,17 +6,12 @@ class Solution {
         
         for (int i = 0; i < arr.length; i++) {
             int len = str.length();
-            if (len == 0) {
-                str += arr[i];
-            } else {
-                if ((arr[i] + "").equals(str.substring(len-1, len))) {
-                    str = str.substring(0, len-1);
-                } else {
-                    str += arr[i];
-                }
+            if (len == 0) str += arr[i];
+            else {
+                if ((arr[i] + "").equals(str.substring(len-1, len))) str = str.substring(0, len-1);
+                else str += arr[i];
             }
         }
-        
         return str.length() == 0 ? new int[]{-1}
             : Arrays.asList(str.split("")).stream().mapToInt(Integer::parseInt).toArray();
     }
